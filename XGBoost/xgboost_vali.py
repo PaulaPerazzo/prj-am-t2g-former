52e3f98531d8711653690a1d44687e7aa93dc5fc
Mariana Paiva <mpb2@cin.ufpe.br>
2025-12-02 18:42:01 -0300

    XGBoost exp

--- XGBoost/xgboost_vali.py
+++ XGBoost/xgboost_vali.py
@@ -44,88 +44,99 @@ for dsfile in sorted(os.listdir(train_path)):
     ds_name = dsfile.replace(".csv", "")
     print(f"Processando: {dsfile}")
 
-    # MÉTRICA A SER OTIMIZADA
     if ds_name in desbalanceados:
         scoring = gmean_scorer
-        otimizado_com = "gmean"
+        best_params_preset = "gbm_xgb"   # ou conforme seu preset/categorização
     else:
         scoring = 'accuracy'
-        otimizado_com = "accuracy"
+        best_params_preset = "gbm_xgb"
 
-    # Ler conjunto de treino e teste
     df_train = pd.read_csv(os.path.join(train_path, dsfile))
     df_test  = pd.read_csv(os.path.join(test_path, dsfile))
     
-    # Supondo que a última coluna é o target
     X_train = df_train.iloc[:, :-1]
     y_train = df_train.iloc[:, -1]
     X_test = df_test.iloc[:, :-1]
     y_test = df_test.iloc[:, -1]
     
-    # --- Adaptação: garantir que os rótulos começam em zero e são numéricos ---
     le = LabelEncoder()
     y_train = le.fit_transform(y_train)
     y_test = le.transform(y_test)
 
-    # Instanciar XGBClassifier
     xgb = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
-
-    # Validação cruzada no conjunto de treino
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
+    # Tempo de tuning (GridSearchCV)
+    start_tune = time.time()
     grid = GridSearchCV(
         estimator=xgb,
         param_grid=param_grid,
         scoring=scoring,
         cv=cv,
         n_jobs=-1,
-        verbose=1
+        verbose=0
     )
-
-    # Cronometrar tuning + fit
-    start_time = time.time()
     grid.fit(X_train, y_train)
-    tempo_total = time.time() - start_time
-
+    tuning_time = time.time() - start_tune
     best_xgb = grid.best_estimator_
 
-    # Métricas em treino
-    y_train_pred = best_xgb.predict(X_train)
-    y_train_pred_proba = best_xgb.predict_proba(X_train)
-    acc_train = accuracy_score(y_train, y_train_pred)
-    try:
-        auc_train = roc_auc_score(y_train, y_train_pred_proba, multi_class='ovo')
-    except:
-        auc_train = np.nan
-    gmean_train = gmean_score(y_train, y_train_pred)
-    ce_train = log_loss(y_train, y_train_pred_proba)
+    # Tempo de treinamento do melhor modelo (refit final)
+    start_train = time.time()
+    best_xgb.fit(X_train, y_train)
+    training_time = time.time() - start_train
 
-    # Métricas em teste
+    # Tempo de predição teste
+    start_pred = time.time()
     y_test_pred = best_xgb.predict(X_test)
     y_test_pred_proba = best_xgb.predict_proba(X_test)
-    acc = accuracy_score(y_test, y_test_pred)
+    predict_time = time.time() - start_pred
+
+    # Tempo total = soma dos tempos
+    total_time = tuning_time + training_time + predict_time
+
+    # Métricas treino
+    y_train_pred = best_xgb.predict(X_train)
+    y_train_pred_proba = best_xgb.predict_proba(X_train)
+    train_metrics_accuracy = accuracy_score(y_train, y_train_pred)
+    train_metrics_gmean = gmean_score(y_train, y_train_pred)
+    train_metrics_cross_entropy = log_loss(y_train, y_train_pred_proba)
     try:
-        auc = roc_auc_score(y_test, y_test_pred_proba, multi_class='ovo')
+        train_metrics_auc_ovo = roc_auc_score(y_train, y_train_pred_proba, multi_class='ovo')
     except:
-        auc = np.nan
-    gmean = gmean_score(y_test, y_test_pred)
-    ce = log_loss(y_test, y_test_pred_proba)
+        train_metrics_auc_ovo = np.nan
+
+    # Métricas teste
+    test_metrics_accuracy = accuracy_score(y_test, y_test_pred)
+    test_metrics_gmean    = gmean_score(y_test, y_test_pred)
+    test_metrics_cross_entropy = log_loss(y_test, y_test_pred_proba)
+    try:
+        test_metrics_auc_ovo = roc_auc_score(y_test, y_test_pred_proba, multi_class='ovo')
+    except:
+        test_metrics_auc_ovo = np.nan
 
     results.append({
         "dataset": dsfile,
-        "mean_accuracy_test": acc,
-        "mean_auc_ovo_test": auc,
-        "gmean_test": gmean,
-        "cross_entropy_test": ce,
-        "mean_accuracy_train": acc_train,
-        "mean_auc_ovo_train": auc_train,
-        "gmean_train": gmean_train,
-        "cross_entropy_train": ce_train,
-        "train_time_sec": tempo_total,
-        "best_params": grid.best_params_,
-        "otimizado_com": otimizado_com
+        "total_time": total_time,
+        "tuning_time": tuning_time,
+        "training_time": training_time,
+        "predict_time": predict_time,
+        "best_params_preset": best_params_preset,
+        "best_params_model_set": grid.best_params_,
+        "train_metrics_accuracy": train_metrics_accuracy,
+        "train_metrics_gmean": train_metrics_gmean,
+        "train_metrics_cross_entropy": train_metrics_cross_entropy,
+        "train_metrics_auc_ovo": train_metrics_auc_ovo,
+        "test_metrics_accuracy": test_metrics_accuracy,
+        "test_metrics_gmean": test_metrics_gmean,
+        "test_metrics_cross_entropy": test_metrics_cross_entropy,
+        "test_metrics_auc_ovo": test_metrics_auc_ovo
     })
 
-df_final = pd.DataFrame(results)
+df_final = pd.DataFrame(results, columns=[
+    "dataset","total_time","tuning_time","training_time","predict_time",
+    "best_params_preset","best_params_model_set",
+    "train_metrics_accuracy","train_metrics_gmean","train_metrics_cross_entropy","train_metrics_auc_ovo",
+    "test_metrics_accuracy","test_metrics_gmean","test_metrics_cross_entropy","test_metrics_auc_ovo"
+])
 df_final.to_csv("xgboost_todos_resultados.csv", index=False)
 print("Resultados salvos em xgboost_todos_resultados.csv")
